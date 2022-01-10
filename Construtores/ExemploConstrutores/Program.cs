0383fe3275616e2888d269404bb1bd2c85b3974c
vinicius <87672761+vinicius1414@users.noreply.github.com>
2022-01-10 09:30:29 -0300

    Add files via upload

---
+++ Construtores/ExemploConstrutores/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ExemploConstrutores.Models;
+
+namespace ExemploConstrutores // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public delegate void Operacao(int x, int y);
+        public static void Main(string[] args)
+        {
+            
+            Matematica m = new Matematica(10, 20);
+            m.Somar();
+
+
+            //Operacao op = Calculadora.Somar;
+            //Operacao op = new Operacao(Calculadora.Somar);
+            //op += Calculadora.Subtrair;
+            //op(10, 10);
+            
+
+        }
+    }
+}
